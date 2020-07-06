1f5f4d312d810a146e5fffaac22c662940317187
Tony <35574936+lackhole@users.noreply.github.com>
2020-07-06 13:13:30 +0900

    update cutemodel to modern C++

--- cutemodel/CuteModel.hpp
+++ cutemodel/CuteModel.hpp
@@ -5,7 +5,7 @@
 #ifndef CUTEMODEL_H
 #define CUTEMODEL_H
 
-#define USE_GPU_DELEGATE 0
+#define USE_GPU_DELEGATE 1
 #define USE_NNAPI_DELEGATE 0
 
 
@@ -29,112 +29,128 @@
 
 namespace ct {
     
-    size_t elementByteSize(const TfLiteTensor *tensor);
-    
-    size_t tensorLength(const TfLiteTensor *tensor);
-    
     class CuteModel {
     public:
-        
-        TfLiteModel *model = nullptr;
-        TfLiteInterpreterOptions *options = nullptr;
-        TfLiteInterpreter *interpreter = nullptr;
-        
-        
     
-        /**
-         * Constructor / Move Con/Op
-         */
-
-//    CuteModel() = delete;
         CuteModel() = default;
+        ~CuteModel() = default;
         
-        ~CuteModel();
+        /** construct from model buffer */
+        CuteModel(const void *buffer, size_t bufferSize);
         
-        CuteModel(void *buffer, size_t bufferSize);
-        
+        /** construct from model file */
         CuteModel(const std::string& path);
         
-        CuteModel(CuteModel &&other);
+        /** Move con,op */
+        CuteModel(CuteModel &&other) noexcept;
+        CuteModel& operator=(CuteModel &&other) noexcept;
         
-        CuteModel &operator=(CuteModel &&other);
+        /** build from model buffer */
+        void buildFromBuffer(const void* buffer, size_t bufferSize);
+        
+        /** build from model file */
+        void buildFromFile(const std::string& path);
         
         /**
-         *
+         * set number of threads
+         * if GPU delegate is used, setting a thread is meaningless
          */
-        
-        
         void setCpuNumThreads(int numThread = -1);
+    
 
-#ifdef TENSORFLOW_LITE_DELEGATES_GPU_DELEGATE_H_
-        TfLiteGpuDelegateOptionsV2 gpuDelegateOptionsV2{};
-        TfLiteDelegate *gpuDelegate = nullptr;
-        
+    #ifdef TENSORFLOW_LITE_DELEGATES_GPU_DELEGATE_H_
+        /**
+         * set GPU delegate
+         * you can customize options; See TfLiteGpuDelegateOptionsV2's declared header
+         */
         void setGpuDelegate(const TfLiteGpuDelegateOptionsV2 &gpuOptions = TfLiteGpuDelegateOptionsV2Default());
-#endif
-
-#ifdef TENSORFLOW_LITE_DELEGATES_NNAPI_NNAPI_DELEGATE_H_
-        tflite::StatefulNnApiDelegate::Options nnApiDelegateOptions{};
-        tflite::StatefulNnApiDelegate *nnApiDelegate = nullptr;
-        
+    #endif
+    
+    #ifdef TENSORFLOW_LITE_DELEGATES_NNAPI_NNAPI_DELEGATE_H_
+        /**
+         * set NNAPI delegate for Android devices
+         */
         void setNnApiDelegate(const tflite::StatefulNnApiDelegate::Options &nnApiOptions = tflite::StatefulNnApiDelegate::Options());
-#endif
+    #endif
         
-        TfLiteInterpreter* buildInterpreter();
+        /**
+         * build the interpreter
+         */
+        void buildInterpreter();
         
-         bool isBuilt() const;
+        /** check if the interpreter is built */
+        bool isBuilt() const noexcept;
         
-        template<typename Data, typename ...Rest>
-        void setInput(const Data &data, const Rest &... data_rest);
+        /** give input data to the model */
+        template<typename Data, typename ...Rest> void setInput(Data &&data, Rest &&... data_rest) noexcept;
+        template<typename Data> void setInput(Data &&data) noexcept;
         
-        template<typename Data>
-        void setInput(const Data &data);
+        /** run an inference */
+        void invoke() noexcept;
         
-        void invoke();
+        /** get specific index of the output data */
+        template<typename T> [[deprecated("use copyOutputToBuffer instead")]] void getOutput(int index, std::vector<T> &output) const noexcept;
+        template<typename T> std::vector<T> getOutput(int index) const noexcept;
         
-        template<typename T>
-        void getOutput(int index, std::vector<T> &output) const;
+        /** copy output data to buffer '
+         * IMPORTANT: buffer must be allocated
+         */
+        template<typename T> void copyOutputToBuffer(int index, T&& buffer) const noexcept;
+        template<typename T> void copyOutputToBuffer(int index, T&& buffer, size_t bufferSize) const noexcept;
         
-        template<typename T>
-        void getOutput(std::vector<std::vector<T>> &output) const;
+        /** return number of i/o tensors */
+        auto inputTensorCount() const;
+        auto outputTensorCount() const;
         
-        template<typename T>
-        std::vector<std::vector<T>> getOutput() const;
-        
-        template<typename T>
-        std::vector<T> getOutput(int index) const;
-        
-        int32_t inputTensorCount() const;
-        int32_t outputTensorCount() const;
-        
+        /** return specific index of tensor's length of an element array */
         size_t inputTensorLength(int index) const;
         size_t outputTensorLength(int index) const;
         
+        /** return specific index of a tensor */
         TfLiteTensor* inputTensor(int index);
         const TfLiteTensor* inputTensor(int index) const;
-        
         const TfLiteTensor* outputTensor(int index) const;
         
+        /** return model's information */
         std::string summary() const;
-
+    
+        /** return inferencing hardware options */
         std::string summarizeOptions() const;
         
     
     private:
+        
+        /** Tensorflow Lite members */
+        std::unique_ptr<TfLiteModel, decltype(&TfLiteModelDelete)> model{nullptr, TfLiteModelDelete};
+        std::unique_ptr<TfLiteInterpreterOptions, decltype(&TfLiteInterpreterOptionsDelete)> options{nullptr, TfLiteInterpreterOptionsDelete};
+        std::unique_ptr<TfLiteInterpreter, decltype(&TfLiteInterpreterDelete)> interpreter{nullptr, TfLiteInterpreterDelete};
+        
+        #ifdef TENSORFLOW_LITE_DELEGATES_GPU_DELEGATE_H_
+        /** Tensorflow Lite GPU delegate members */
+        TfLiteGpuDelegateOptionsV2 gpuDelegateOptionsV2{};
+        std::unique_ptr<TfLiteDelegate, decltype(&TfLiteGpuDelegateV2Delete)> gpuDelegate{nullptr, TfLiteGpuDelegateV2Delete};
+        #endif
+        
+        #ifdef TENSORFLOW_LITE_DELEGATES_NNAPI_NNAPI_DELEGATE_H_
+        /** Tensorflow Lite NNAPI delegate members */
+        tflite::StatefulNnApiDelegate::Options nnApiDelegateOptions{};
+        std::unique_ptr<tflite::StatefulNnApiDelegate> nnApiDelegate(nullptr);
+        #endif
+        
+        /** auto incrementing input index */
         int input_data_index = 0;
         
-        void clear();
+//        /** i/o tensor byte storage */
+//        std::vector<size_t> inputTensorByteArray;
+//        std::vector<size_t> outputTensorByteArray;
         
         CuteModel(const CuteModel &other) = delete;
-        
-        void operator=(const CuteModel &other) = delete;
+        CuteModel& operator=(const CuteModel &other) = delete;
     };
     
     
     template<typename Data>
-    void CuteModel::setInput(const Data &data) {
-        assert(("Too many input to model", input_data_index < inputTensorCount()));
-        
+    inline void CuteModel::setInput(Data &&data) noexcept {
         TfLiteTensorCopyFromBuffer(
                 inputTensor(input_data_index),
                 data,
@@ -144,9 +160,7 @@ namespace ct {
     }
     
     template<typename Data, typename ...Rest>
-    void CuteModel::setInput(const Data &data, const Rest &... data_rest) {
-        assert(("Too many input to model", input_data_index < inputTensorCount()));
-        
+    inline void CuteModel::setInput(Data &&data, Rest &&... data_rest) noexcept {
         TfLiteTensorCopyFromBuffer(
                 inputTensor(input_data_index),
                 data,
@@ -156,49 +170,37 @@ namespace ct {
         setInput(data_rest...);
     }
     
-    template<typename T>
-    void CuteModel::getOutput(std::vector<std::vector<T>> &output) const {
-        if(!output.empty())
-            output.clear();
-        
-        output.resize(outputTensorCount());
-        for(int i = 0; i < output.size(); ++i) {
-            output[i].resize((TfLiteTensorByteSize(outputTensor(i)) / sizeof(T)));
-            TfLiteTensorCopyToBuffer(outputTensor(i), output[i].data(),
-                                     TfLiteTensorByteSize(outputTensor(i)));
-        }
+    inline void CuteModel::invoke() noexcept {
+        TfLiteInterpreterInvoke(interpreter.get());
+        input_data_index = 0;
     }
     
     template<typename T>
-    void CuteModel::getOutput(int index, std::vector<T> &output) const {
-        if(!output.empty())
-            output.clear();
+    inline void CuteModel::getOutput(int index, std::vector<T> &output) const noexcept {
+        static_assert(!std::is_class<T>::value, "vector must not contain classes");
         
-        output.resize((TfLiteTensorByteSize(outputTensor(index)) / sizeof(T)));
-        TfLiteTensorCopyToBuffer(outputTensor(index), output.data(),
-                                 TfLiteTensorByteSize(outputTensor(index)));
-    }
-    
-    template<typename T>
-    std::vector<std::vector<T>> CuteModel::getOutput() const {
-        std::vector<std::vector<T>> output(outputTensorCount());
-        
-        for (int i=0; i<output.size(); ++i){
-            output[i].resize((TfLiteTensorByteSize(outputTensor(i)) / sizeof(T)));
-            TfLiteTensorCopyToBuffer(outputTensor(i), output[i].data(),
-                                     TfLiteTensorByteSize(outputTensor(i)));
-        }
-        
-        return std::move(output);
+        output.resize((TfLiteTensorByteSize(outputTensor(index)) / sizeof(T))); // std::vector 안에서 자동으로 크기 체크함
+        TfLiteTensorCopyToBuffer(outputTensor(index), output.data(), TfLiteTensorByteSize(outputTensor(index)));
     }
     
     template <typename T>
-    std::vector<T> CuteModel::getOutput(int index) const {
+    inline std::vector<T> CuteModel::getOutput(int index) const noexcept {
+        static_assert(!std::is_class<T>::value, "vector must not contain classes");
+        
         std::vector<T> output((TfLiteTensorByteSize(outputTensor(index)) / sizeof(T)));
-        TfLiteTensorCopyToBuffer(outputTensor(index), output.data(),
-                                 TfLiteTensorByteSize(outputTensor(index)));
+        TfLiteTensorCopyToBuffer(outputTensor(index), output.data(), TfLiteTensorByteSize(outputTensor(index)));
         
         return std::move(output);
+    }
+    
+    template<typename T>
+    inline void CuteModel::copyOutputToBuffer(int index, T &&buffer) const noexcept {
+        copyOutputToBuffer(index, buffer, TfLiteTensorByteSize(outputTensor(index)));
+    }
+    
+    template<typename T>
+    inline void CuteModel::copyOutputToBuffer(int index, T &&buffer, size_t bufferSize) const noexcept {
+        TfLiteTensorCopyToBuffer(outputTensor(index), buffer, bufferSize);
     }
     
 }
